490bef0107905461cb1bbf4310383a94a0b21852
Luis Felipe S Botta <felipeex7@gmail.com>
2025-07-19 15:49:47 -0300

    .

---
+++ ConvencoesCodigo/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+}
+
+fun soma(n1: Int, n2: Int): Int {
+    return n1 + n2
+}
+
+fun divisao(n1: Float, n2: Float): Float {
+    val s = "sdadfsdgasdfgafgafdgafdgafdgfa" +
+            "saasdasdasddasdsaasdsadasdsdadsa" +
+            "dsaasdsadsdaasddsasadasdsadsaddg"
+    if (n2 == 0F) {
+        return 0F
+    }
+    return n1 / n2
+}
