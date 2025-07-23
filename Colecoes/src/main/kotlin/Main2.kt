2f49b34a6b24d460380cc1a2b28ed2ca6980396e
Luis Felipe S Botta <felipeex7@gmail.com>
2025-07-23 19:26:41 -0300

    .

--- Colecoes/src/main/kotlin/Main2.kt
+++ Colecoes/src/main/kotlin/Main2.kt
@@ -1,3 +1,8 @@
+fun abc(r: Receita): Boolean {
+    return (r.calorias > 500)
+}
+
+
 fun main() {
 
     val dados = listaDeDados()
@@ -7,6 +12,8 @@ fun main() {
     // count retorna a quantidade de itens dentro da lista
     // filter cria um retorno através de um escopo de função (utilizar it. para abrir
     // as opções para utilizar o filtro
+
+    println(dados.filter { abc(it) })
 
     println(dados.filter { it.calorias > 500 })
     println(dados.any { it.calorias > 500 })
@@ -21,27 +28,49 @@ fun main() {
     dados.last { it.calorias > 500 } // utlizando o escopo de função
 
     // sum, sumOf
-    listOf(1,2,3).sum()
+    listOf(1, 2, 3).sum()
     dados.sumOf { it.calorias }
 
     val abc = dados.filter { it.calorias > 500 }.sumOf { it.calorias }
 
     // max, min deprecated (depreciado, existe alguma funcionalidade que a substitui)
-    listOf(1,2,3).max()
-    listOf(1,2,3).min()
+    listOf(1, 2, 3).max()
+    listOf(1, 2, 3).min()
 
     // a funcinalidade que substitui o max e min
-    listOf(1,2,3).maxOrNull() 
-    listOf(1,2,3).minOrNull()
+    listOf(1, 2, 3).maxOrNull()
+    listOf(1, 2, 3).minOrNull()
 
     dados.maxOf { it.calorias } // vai retornar a maior caloria
     dados.minOf { it.calorias }
 
-    dados.maxByOrNull { it.calorias } // vai retornar toda a receita
+    println(dados.maxByOrNull { it.calorias }?.nome) // vai retornar toda a receita
+
+    // take, takeLast, forEach
+
+    dados.take(2).forEach { println(it.nome) }
+
+    // map
+
+    println(dados.map { it.nome })
+    println(dados.map { Pair(it.nome, it.calorias) })
+
+    // average, distinct, sorted e reversed
+
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 10)
+    numbers.average()
+    println(numbers.distinct()) // remove os números duplicados
+    dados.distinctBy { it.nome } // remove os itens duplicados pela chave informada
+    println(numbers.sorted()) // retorna a lista ordenada
+    println(numbers.sortedDescending()) // retorna a lista ordenada de forma decrescente
+    println(numbers.reversed()) // retorna uma lista na ordem inversa
+
+
+    dados.filter { it.nome.startsWith("P") }.map { it.nome }.sorted().forEach { println(it.uppercase()) }
 
 }
 
-data class Receita(val nome: String, val calorias:Int, val ingredientes: List<Ingrediente> = listOf())
+data class Receita(val nome: String, val calorias: Int, val ingredientes: List<Ingrediente> = listOf())
 data class Ingrediente(val nome: String, val quantidade: Int)
 
 /**
